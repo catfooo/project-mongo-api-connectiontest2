6bb8e71b2d82096966bef0259533317813568ddc
So Youn Choi <manghatuhyo@gmail.com>
2023-12-16 12:48:39 +0100

    installed dotenv and made require dotenv config, but having error saying there is timeout and crashing afterwards

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import listEndpoints from 'express-list-endpoints'
+require('dotenv').config()
 
 // If you're using one of our datasets, uncomment the appropriate import below
 // to get started!
