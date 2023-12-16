ec327fefb175771cc50a25487f1232275bdbe28e
So Youn Choi <manghatuhyo@gmail.com>
2023-12-16 12:18:01 +0100

    added few lines to see the connection

--- server.js
+++ server.js
@@ -11,12 +11,14 @@ import booksData from "./data/books.json";
 // import netflixData from "./data/netflix-titles.json";
 // import topMusicData from "./data/top-music.json";
 
+console.log("Before connecting to MongoDB")
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 console.log(`Connecting to MongoDB using URL: ${mongoUrl}`)
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.error("MongoDB connection error:", err))
 mongoose.Promise = Promise;
+console.log("After connecting to MongoDB")
 
 const Book = mongoose.model('Book', {
   bookID: Number,
